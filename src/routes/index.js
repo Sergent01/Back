5a82132e87023597ac689368ab21d49de71bd084
Léo Peyré <leo.peyre95@gmail.com>
2022-08-30 12:09:45 +0200

    Survey complet

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+
 const userRouter = require('./user.routes');
+const surveyRouter = require('./survey.routes')
 
 router.use(userRouter);
+router.use(surveyRouter)
 
 module.exports = router;
